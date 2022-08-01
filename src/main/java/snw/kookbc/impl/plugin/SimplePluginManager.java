4c7b508609b2832a333ca7af5f2a285e01c4ee4a
ZX夏夜之风 <snwcreations@qq.com>
2022-08-01 18:15:22 +0800

    Implement PluginManager from JKook 0.28.0

---
+++ src/main/java/snw/kookbc/impl/plugin/SimplePluginManager.java
@@ -0,0 +1,160 @@
+/*
+ *     KookBC -- The Kook Bot Client & JKook API standard implementation for Java.
+ *     Copyright (C) 2022 KookBC contributors
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published
+ *     by the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package snw.kookbc.impl.plugin;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import snw.jkook.plugin.*;
+import snw.jkook.util.Validate;
+import snw.kookbc.impl.KBCClient;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Objects;
+
+import static snw.kookbc.util.Util.getVersionDifference;
+
+public class SimplePluginManager implements PluginManager {
+    private final KBCClient client;
+    private final Collection<Plugin> plugins = new ArrayList<>();
+    private final PluginClassLoader pluginClassLoader;
+
+    public SimplePluginManager(KBCClient client) {
+        this.client = client;
+        pluginClassLoader = new SimplePluginClassLoader(client);
+    }
+
+    @Override
+    public @Nullable Plugin getPlugin(String name) {
+        return plugins.stream()
+                .filter(IT -> Objects.equals(IT.getDescription().getName(), name))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public Plugin[] getPlugins() {
+        return getPlugins0().toArray(new Plugin[0]);
+    }
+
+    public Collection<Plugin> getPlugins0() {
+        return plugins;
+    }
+
+    @Override
+    public boolean isPluginEnabled(String name) {
+        Plugin plugin = getPlugin(name);
+        return plugin != null && plugin.isEnabled();
+    }
+
+    @Override
+    public boolean isPluginEnabled(Plugin plugin) {
+        return plugin.isEnabled();
+    }
+
+    @Override
+    public @NotNull Plugin loadPlugin(File file) throws InvalidPluginException {
+        Plugin plugin;
+        try {
+            plugin = pluginClassLoader.loadPlugin(file);
+            PluginDescription description = plugin.getDescription();
+            int diff = getVersionDifference(description.getApiVersion(), client.getCore().getAPIVersion());
+            if (diff == -1) {
+                plugin.getLogger().warn("The plugin is using old version of JKook API! We are using {}, got {}", client.getCore().getAPIVersion(), description.getApiVersion());
+            }
+            if (diff == 1) {
+                throw new IllegalArgumentException(String.format("The plugin is using unsupported version of JKook API! We are using %s, got %s", client.getCore().getAPIVersion(), description.getApiVersion()));
+            }
+        } catch (Exception e) {
+            if (e instanceof InvalidPluginException) {
+                throw (InvalidPluginException) e;
+            } else {
+                throw new InvalidPluginException(e);
+            }
+        }
+        return plugin;
+    }
+
+    @Override
+    public @NotNull Plugin[] loadPlugins(File directory) {
+        Collection<Plugin> plugins = new ArrayList<>();
+        Validate.isTrue(directory.isDirectory(), "The provided file object is not a directory.");
+        File[] files = directory.listFiles(pathname -> pathname.getName().endsWith(".jar"));
+        if (files != null) {
+            // we must call onLoad() first.
+            for (File file : files) {
+                Plugin plugin;
+                try {
+                    plugin = loadPlugin(file);
+                } catch (Exception e) {
+                    client.getCore().getLogger().error("Unable to load a plugin.", e);
+                    continue;
+                }
+                plugins.add(plugin);
+            }
+        }
+        return plugins.toArray(new Plugin[0]);
+    }
+
+    @Override
+    public void disablePlugins() {
+        for (Plugin plugin : plugins) {
+            disablePlugin(plugin);
+        }
+    }
+
+    @Override
+    public void clearPlugins() {
+        disablePlugins();
+        plugins.clear();
+    }
+
+    @Override
+    public void enablePlugin(Plugin plugin) {
+        if (isPluginEnabled(plugin)) return;
+        PluginDescription description = plugin.getDescription();
+        plugin.getLogger().info("Enabling {} version {}", description.getName(), description.getVersion());
+        try {
+            plugin.setEnabled(true);
+        } catch (Exception e) {
+            client.getCore().getLogger().error("Exception occurred while we attempting to enable the {} plugin.", plugin.getDescription().getName(), e);
+        }
+    }
+
+    @Override
+    public void disablePlugin(Plugin plugin) {
+        if (!isPluginEnabled(plugin)) return;
+        PluginDescription description = plugin.getDescription();
+        plugin.getLogger().info("Disabling {} version {}", description.getName(), description.getVersion());
+        try {
+            plugin.setEnabled(false);
+        } catch (Exception e) {
+            client.getCore().getLogger().error("Exception occurred while we attempting to disable the {} plugin.", plugin.getDescription().getName(), e);
+        }
+        if (plugin.getClass().getClassLoader() instanceof SimplePluginClassLoader) {
+            try {
+                ((SimplePluginClassLoader) plugin.getClass().getClassLoader()).close();
+            } catch (IOException e) {
+                client.getCore().getLogger().error("Unexpected IOException while we attempting to close the PluginClassLoader.", e);
+            }
+        }
+    }
+}
