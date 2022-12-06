0b3088ed9fdcf1ae14a9b9ebc7cbf3914e560450
ZX夏夜之风 <snwcreations@qq.com>
2022-12-06 12:48:55 +0800

    Add Bucket name mapping.

--- src/main/java/snw/kookbc/impl/network/Bucket.java
+++ src/main/java/snw/kookbc/impl/network/Bucket.java
@@ -75,6 +75,80 @@ public class Bucket {
     }
 
     static {
-        // TODO put Route->BucketName mapping here.
+        // Bucket name mapping is here.
+        // Why I wrote this?
+        // Although these known API have their own bucket,
+        // but we do not know if the API from future will
+        // use the bucket of another API.
+        // So the mapping is important.
+        bucketNameMap.put(HttpAPIRoute.GUILD_JOINED_LIST, "guild/list");
+        bucketNameMap.put(HttpAPIRoute.GUILD_INFO, "guild/view");
+        bucketNameMap.put(HttpAPIRoute.GUILD_USERS, "guild/user-list");
+        bucketNameMap.put(HttpAPIRoute.GUILD_CHANGE_OTHERS_NICKNAME, "guild/nickname");
+        bucketNameMap.put(HttpAPIRoute.GUILD_LEAVE, "guild/leave");
+        bucketNameMap.put(HttpAPIRoute.GUILD_KICK, "guild/kickout");
+        bucketNameMap.put(HttpAPIRoute.MUTE_LIST, "guild-mute/list");
+        bucketNameMap.put(HttpAPIRoute.MUTE_ADD, "guild-mute/create");
+        bucketNameMap.put(HttpAPIRoute.MUTE_REMOVE, "guild-mute/delete");
+        bucketNameMap.put(HttpAPIRoute.CHANNEL_LIST, "channel/list");
+        bucketNameMap.put(HttpAPIRoute.CHANNEL_INFO, "channel/view");
+        bucketNameMap.put(HttpAPIRoute.CHANNEL_CREATE, "channel/create");
+        bucketNameMap.put(HttpAPIRoute.CHANNEL_UPDATE, "channel/update");
+        bucketNameMap.put(HttpAPIRoute.CHANNEL_DELETE, "channel/delete");
+        bucketNameMap.put(HttpAPIRoute.CHANNEL_USER_LIST, "channel/user-list");
+        bucketNameMap.put(HttpAPIRoute.MOVE_USER, "channel/move-user");
+        bucketNameMap.put(HttpAPIRoute.CHANNEL_ROLE_CREATE, "channel-role/create");
+        bucketNameMap.put(HttpAPIRoute.CHANNEL_ROLE_UPDATE, "channel-role/update");
+        bucketNameMap.put(HttpAPIRoute.CHANNEL_ROLE_DELETE, "channel-role/delete");
+        bucketNameMap.put(HttpAPIRoute.CHANNEL_MESSAGE_QUERY, "message/list");
+        bucketNameMap.put(HttpAPIRoute.CHANNEL_MESSAGE_INFO, "message/view");
+        bucketNameMap.put(HttpAPIRoute.CHANNEL_MESSAGE_SEND, "message/create");
+        bucketNameMap.put(HttpAPIRoute.CHANNEL_MESSAGE_UPDATE, "message/update");
+        bucketNameMap.put(HttpAPIRoute.CHANNEL_MESSAGE_DELETE, "message/delete");
+        bucketNameMap.put(HttpAPIRoute.CHANNEL_MESSAGE_REACTION_LIST, "message/reaction-list");
+        bucketNameMap.put(HttpAPIRoute.CHANNEL_MESSAGE_REACTION_ADD, "message/add-reaction");
+        bucketNameMap.put(HttpAPIRoute.CHANNEL_MESSAGE_REACTION_REMOVE, "message/delete-reaction");
+        bucketNameMap.put(HttpAPIRoute.CHANNEL_USER_VOICE_CHANNEL, "channel-user/get-joined-channel");
+        bucketNameMap.put(HttpAPIRoute.USER_CHAT_LIST, "user-chat/list");
+        bucketNameMap.put(HttpAPIRoute.USER_CHAT_INFO, "user-chat/view");
+        bucketNameMap.put(HttpAPIRoute.USER_CHAT_SESSION_CREATE, "user-chat/create");
+        bucketNameMap.put(HttpAPIRoute.USER_CHAT_SESSION_DELETE, "user-chat/delete");
+        bucketNameMap.put(HttpAPIRoute.USER_CHAT_MESSAGE_LIST, "direct-message/list");
+        bucketNameMap.put(HttpAPIRoute.USER_CHAT_MESSAGE_CREATE, "direct-message/create");
+        bucketNameMap.put(HttpAPIRoute.USER_CHAT_MESSAGE_UPDATE, "direct-message/update");
+        bucketNameMap.put(HttpAPIRoute.USER_CHAT_MESSAGE_DELETE, "direct-message/delete");
+        bucketNameMap.put(HttpAPIRoute.USER_CHAT_MESSAGE_REACTION_LIST, "direct-message/reaction-list");
+        bucketNameMap.put(HttpAPIRoute.USER_CHAT_MESSAGE_REACTION_ADD, "direct-message/add-reaction");
+        bucketNameMap.put(HttpAPIRoute.USER_CHAT_MESSAGE_REACTION_REMOVE, "direct-message/delete-reaction");
+        bucketNameMap.put(HttpAPIRoute.GATEWAY, "gateway/index");
+        bucketNameMap.put(HttpAPIRoute.USER_ME, "user/me");
+        bucketNameMap.put(HttpAPIRoute.USER_WHO, "user/view");
+        bucketNameMap.put(HttpAPIRoute.USER_BOT_OFFLINE, "user/offline");
+        bucketNameMap.put(HttpAPIRoute.ASSET_UPLOAD, "asset/create");
+        bucketNameMap.put(HttpAPIRoute.ROLE_LIST, "guild-role/list");
+        bucketNameMap.put(HttpAPIRoute.ROLE_CREATE, "guild-role/create");
+        bucketNameMap.put(HttpAPIRoute.ROLE_UPDATE, "guild-role/update");
+        bucketNameMap.put(HttpAPIRoute.ROLE_DELETE, "guild-role/delete");
+        bucketNameMap.put(HttpAPIRoute.ROLE_GRANT, "guild-role/grant");
+        bucketNameMap.put(HttpAPIRoute.ROLE_REVOKE, "guild-role/revoke");
+        bucketNameMap.put(HttpAPIRoute.INTIMACY_INFO, "intimacy/index");
+        bucketNameMap.put(HttpAPIRoute.INTIMACY_UPDATE, "intimacy/update");
+        bucketNameMap.put(HttpAPIRoute.GUILD_EMOJI_LIST, "guild-emoji/list");
+        bucketNameMap.put(HttpAPIRoute.GUILD_EMOJI_CREATE, "guild-emoji/create");
+        bucketNameMap.put(HttpAPIRoute.GUILD_EMOJI_UPDATE, "guild-emoji/update");
+        bucketNameMap.put(HttpAPIRoute.GUILD_EMOJI_DELETE, "guild-emoji/delete");
+        bucketNameMap.put(HttpAPIRoute.INVITE_LIST, "invite/list");
+        bucketNameMap.put(HttpAPIRoute.INVITE_CREATE, "invite/create");
+        bucketNameMap.put(HttpAPIRoute.INVITE_DELETE, "invite/delete");
+        bucketNameMap.put(HttpAPIRoute.BLACKLIST_LIST, "blacklist/list");
+        bucketNameMap.put(HttpAPIRoute.BLACKLIST_CREATE, "blacklist/create");
+        bucketNameMap.put(HttpAPIRoute.BLACKLIST_DELETE, "blacklist/delete");
+        bucketNameMap.put(HttpAPIRoute.GAME_LIST, "game");
+        bucketNameMap.put(HttpAPIRoute.GAME_CREATE, "game/create");
+        bucketNameMap.put(HttpAPIRoute.GAME_UPDATE, "game/update");
+        bucketNameMap.put(HttpAPIRoute.GAME_DELETE, "game/delete");
+        bucketNameMap.put(HttpAPIRoute.GAME_CREATE_ACTIVITY, "game/activity");
+        bucketNameMap.put(HttpAPIRoute.GAME_DELETE_ACTIVITY, "game/delete-activity");
+        // bucketNameMap.put(HttpAPIRoute.GUILD_BOOST_HISTORY, "guild-boost/history");
     }
 }
