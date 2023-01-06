131fa5c8fdf61c3e1f92ae3856629eff15574ebf
ZX夏夜之风 <snwcreations@qq.com>
2023-01-06 01:15:08 +0000

    Let MethodScannerImpl cannot be constructed by outer code.

--- src/main/java/snw/kookbc/impl/event/MethodScannerImpl.java
+++ src/main/java/snw/kookbc/impl/event/MethodScannerImpl.java
@@ -31,6 +31,9 @@ import snw.jkook.event.Listener;
 public final class MethodScannerImpl implements MethodScanner<Listener> {
     public static final MethodScannerImpl INSTANCE = new MethodScannerImpl();
 
+    private MethodScannerImpl() {
+    }
+
     @Override
     public boolean shouldRegister(@NonNull Listener listener, @NonNull Method method) {
         return Modifier.isPublic(method.getModifiers()) && method.isAnnotationPresent(EventHandler.class) && method.getParameterCount() == 1 && !Modifier.isAbstract(method.getParameterTypes()[0].getModifiers());
