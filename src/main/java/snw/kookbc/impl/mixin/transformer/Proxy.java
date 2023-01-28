2109c6616942a8838eca9705bfe7ce86a4302fdb
ZX夏夜之风 <snwcreations@qq.com>
2023-01-28 16:07:25 +0800

    Add final descriptor for two fields in Proxy class
    Suggested by IntelliJ IDEA

--- src/main/java/snw/kookbc/impl/mixin/transformer/Proxy.java
+++ src/main/java/snw/kookbc/impl/mixin/transformer/Proxy.java
@@ -44,12 +44,12 @@ public final class Proxy implements IClassTransformer, ILegacyClassTransformer {
     /**
      * All existing proxies
      */
-    private static List<Proxy> proxies = new ArrayList<>();
+    private static final List<Proxy> proxies = new ArrayList<>();
 
     /**
      * Actual mixin transformer instance
      */
-    private static MixinTransformer transformer = new MixinTransformer();
+    private static final MixinTransformer transformer = new MixinTransformer();
 
     /**
      * True if this is the active proxy, newer proxies disable their older
