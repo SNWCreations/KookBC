95be5ac352d8ad2f493d1807da71e04d31e9fe81
ZX夏夜之风 <snwcreations@qq.com>
2023-04-01 14:48:43 +0800

    Rename method argument name to fully match the HttpAPI#addFriend signature.

--- src/main/java/snw/kookbc/impl/HttpAPIImpl.java
+++ src/main/java/snw/kookbc/impl/HttpAPIImpl.java
@@ -234,15 +234,15 @@ public class HttpAPIImpl implements HttpAPI {
         }}
 
     @Override
-    public void addFriend(String userCode, int from, String guildId) {
-        if (from == 2) {
+    public void addFriend(String userCode, int method, String guildId) {
+        if (method == 2) {
             if (guildId == null) {
                 throw new IllegalArgumentException("Guild ID should be NOT NULL if method is 2");
             }
         }
         MapBuilder builder = new MapBuilder()
                 .put("user_code", userCode)
-                .put("from", from);
+                .put("from", method);
         if (guildId != null) {
             builder.put("guild_id", guildId);
         }
