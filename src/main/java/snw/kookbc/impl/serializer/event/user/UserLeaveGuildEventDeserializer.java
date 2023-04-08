08f7a139442d2892ba579abe559a14662c9c27ce
ZX夏夜之风 <snwcreations@qq.com>
2023-04-08 19:55:23 +0800

    Fix issue in UserLeaveGuildEventDeserializer

--- src/main/java/snw/kookbc/impl/serializer/event/user/UserLeaveGuildEventDeserializer.java
+++ src/main/java/snw/kookbc/impl/serializer/event/user/UserLeaveGuildEventDeserializer.java
@@ -18,15 +18,17 @@
 
 package snw.kookbc.impl.serializer.event.user;
 
-import java.lang.reflect.Type;
-
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
-
+import snw.jkook.entity.User;
 import snw.jkook.event.user.UserLeaveGuildEvent;
 import snw.kookbc.impl.KBCClient;
 import snw.kookbc.impl.serializer.event.NormalEventDeserializer;
+
+import java.lang.reflect.Type;
+
+import static snw.kookbc.util.GsonUtil.get;
 
 public class UserLeaveGuildEventDeserializer extends NormalEventDeserializer<UserLeaveGuildEvent> {
 
@@ -36,9 +38,16 @@ public class UserLeaveGuildEventDeserializer extends NormalEventDeserializer<Use
 
     @Override
     protected UserLeaveGuildEvent deserialize(JsonObject object, Type type, JsonDeserializationContext ctx, long timeStamp, JsonObject body) throws JsonParseException {
+        String realType = get(get(object, "extra").getAsJsonObject(), "type").getAsString();
+        User user;
+        if ("self_exited_guild".equals(realType)) {
+            user = client.getCore().getUser();
+        } else {
+            user = client.getStorage().getUser(get(body, "user_id").getAsString());
+        }
         return new UserLeaveGuildEvent(
             timeStamp,
-            client.getCore().getUser(),
+            user,
             client.getStorage().getGuild(body.get("guild_id").getAsString())
         );
     }
