9ce8ef60cde37d10db82c1ffc4c68ac0eeb6fbe6
huanmeng_qwq <49704373+huanmeng-qwq@users.noreply.github.com>
2024-02-13 20:28:47 +0800

    Cloud -> LiteCommands (#63)
    In this commit we had removed everything about Cloud, and switched to another command framework called LiteCommands.
    
    Reviewed-by: xiaoACE6716, SNWCreations
    Tested-by: huanmeng_qwq

--- src/main/java/snw/kookbc/impl/KBCClient.java
+++ src/main/java/snw/kookbc/impl/KBCClient.java
@@ -32,10 +32,10 @@ import snw.jkook.plugin.PluginDescription;
 import snw.jkook.plugin.UnknownDependencyException;
 import snw.jkook.util.Validate;
 import snw.kookbc.SharedConstants;
-import snw.kookbc.impl.command.cloud.CloudCommandManagerImpl;
-import snw.kookbc.impl.command.internal.CloudHelpCommand;
+import snw.kookbc.impl.command.CommandManagerImpl;
 import snw.kookbc.impl.command.internal.HelpCommand;
-import snw.kookbc.impl.command.internal.PluginsCommand;
+import snw.kookbc.impl.command.litecommands.LiteKookFactory;
+import snw.kookbc.impl.command.litecommands.internal.PluginsCommand;
 import snw.kookbc.impl.console.Console;
 import snw.kookbc.impl.entity.builder.EntityBuilder;
 import snw.kookbc.impl.entity.builder.MessageBuilder;
@@ -129,7 +129,7 @@ public class KBCClient {
         this.internalPlugin = new InternalPlugin(this);
         this.core.init(this);
         /*Cloud*/
-        this.commandManager = Optional.ofNullable(commandManager).orElseGet(() -> CloudCommandManagerImpl::new).apply(this);
+        this.commandManager = Optional.ofNullable(commandManager).orElseGet(() -> CommandManagerImpl::new).apply(this);
         this.networkClient = Optional.ofNullable(networkClient).orElseGet(() -> c -> new NetworkClient(c, token)).apply(this);
         this.storage = Optional.ofNullable(storage).orElseGet(() -> EntityStorage::new).apply(this);
         this.entityBuilder = Optional.ofNullable(entityBuilder).orElseGet(() -> EntityBuilder::new).apply(this);
@@ -516,24 +516,23 @@ public class KBCClient {
     }
 
     protected void registerPluginsCommand() {
-        new JKookCommand("plugins")
-                .setDescription("获取已安装到此 " + SharedConstants.IMPL_NAME + " 实例的插件列表。")
-                .setExecutor(new PluginsCommand(this))
-                .register(getInternalPlugin());
+//        new JKookCommand("plugins")
+//                .setDescription("获取已安装到此 " + SharedConstants.IMPL_NAME + " 实例的插件列表。")
+//                .setExecutor(new PluginsCommand(this))
+//                .register(getInternalPlugin());
+        LiteKookFactory.builder(getInternalPlugin())
+                .commands(new PluginsCommand(this))
+                .build()
+        ;
     }
 
     protected void registerHelpCommand() {
-        if (core.getCommandManager() instanceof CloudCommandManagerImpl) {
-            ((CloudCommandManagerImpl) core.getCommandManager())
-                    .registerCloudCommand(internalPlugin, new CloudHelpCommand(this));
-        } else {
-            HelpCommand executor = new HelpCommand(this);
-            new JKookCommand("help")
-                    .setDescription("获取此帮助列表。")
-                    .executesUser(executor)
-                    .executesConsole(executor)
-                    .register(getInternalPlugin());
-        }
+        HelpCommand executor = new HelpCommand(this);
+        new JKookCommand("help")
+                .setDescription("获取此帮助列表。")
+                .executesUser(executor)
+                .executesConsole(executor)
+                .register(getInternalPlugin());
         this.core.getEventManager()
                 .registerHandlers(this.internalPlugin, new UserClickButtonListener(this));
     }
---
+++ src/main/java/snw/kookbc/impl/command/litecommands/KookLitePlatform.java
@@ -0,0 +1,77 @@
+/*
+ *     KookBC -- The Kook Bot Client & JKook API standard implementation for Java.
+ *     Copyright (C) 2022 - 2023 KookBC contributors
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published
+ *     by the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package snw.kookbc.impl.command.litecommands;
+
+import dev.rollczi.litecommands.command.CommandRoute;
+import dev.rollczi.litecommands.meta.Meta;
+import dev.rollczi.litecommands.meta.MetaKey;
+import dev.rollczi.litecommands.meta.MetaType;
+import dev.rollczi.litecommands.platform.AbstractPlatform;
+import dev.rollczi.litecommands.platform.PlatformInvocationListener;
+import dev.rollczi.litecommands.platform.PlatformSuggestionListener;
+import org.jetbrains.annotations.NotNull;
+import snw.jkook.command.CommandExecutor;
+import snw.jkook.command.CommandSender;
+import snw.jkook.command.JKookCommand;
+import snw.jkook.plugin.Plugin;
+import snw.kookbc.impl.command.CommandMap;
+import snw.kookbc.impl.command.WrappedCommand;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class KookLitePlatform extends AbstractPlatform<CommandSender, LiteKookSettings> {
+    public static final MetaKey<Set<String>> PREFIX = MetaKey.of("kookbc_prefix", MetaType.set(), new HashSet<>(Collections.singletonList("/")));
+
+    private final Plugin plugin;
+    private final CommandMap commandMap;
+
+    protected KookLitePlatform(@NotNull LiteKookSettings settings, Plugin plugin, CommandMap commandMap) {
+        super(settings);
+        this.plugin = plugin;
+        this.commandMap = commandMap;
+    }
+
+    @Override
+    protected void hook(CommandRoute<CommandSender> commandRoute, PlatformInvocationListener<CommandSender> platformInvocationListener, PlatformSuggestionListener<CommandSender> platformSuggestionListener) {
+        for (String label : commandRoute.names()) {
+            List<String> desc = commandRoute.meta().get(Meta.DESCRIPTION);
+            Set<String> prefixes = commandRoute.meta().get(PREFIX);
+            // List<String> perms = commandRoute.meta().get(Meta.PERMISSIONS);
+            JKookCommand command = new JKookCommand(label, prefixes)
+                    .setDescription(String.join("\n", desc))
+                    .setExecutor(new LiteKookCommandExecutor(plugin.getCore(), settings, commandRoute, label, platformInvocationListener, platformSuggestionListener));
+            commandMap.register(plugin, command);
+        }
+    }
+
+    @Override
+    protected void unhook(CommandRoute<CommandSender> commandRoute) {
+        for (WrappedCommand wrappedCommand : commandMap.getView(false).values()) {
+            CommandExecutor executor = wrappedCommand.getCommand().getExecutor();
+            if (executor instanceof LiteKookCommandExecutor) {
+                if (commandRoute.isNameOrAlias(wrappedCommand.getCommand().getRootName())) {
+                    commandMap.unregister(wrappedCommand.getCommand());
+                }
+            }
+        }
+    }
+}
